12e33c65ff4606b8688104c481482d2954328bc1
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-04-14 21:27:16 +0900

    commit

--- main.go
+++ main.go
@@ -1,42 +1,26 @@
 package main
 
 import (
-	"context"
+	"AWS/s3"
 	"fmt"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
 )
 
 func main() {
-	// Specify AWS region
+	info := s3.Info{}
+
+	accessKey := "AKIAVOZYFWFTBWEOBG7T"
+	secretKey := "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"
 	region := "ap-northeast-2"
 
-	// Load AWS SDK config with region
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(region))
+	info.Set_session(accessKey, secretKey, region)
+
+	err := info.Set_s3_config()
 	if err != nil {
-		fmt.Println("Error loading AWS SDK config:", err)
-		return
+		fmt.Println(err)
 	}
 
-	// Create IAM client
-	client := iam.NewFromConfig(cfg)
-
-	// Set IAM user name
-	userName := "ann"
-
-	// Create new access key for specified IAM user
-	resp, err := client.CreateAccessKey(context.TODO(), &iam.CreateAccessKeyInput{
-		UserName: aws.String(userName),
-	})
+	err = info.Get_s3_bucket_list()
 	if err != nil {
-		fmt.Println("Error creating access key for IAM user:", err)
-		return
+		fmt.Println(err)
 	}
-
-	// Print access key ID and secret access key
-	fmt.Println("Access key ID:", *resp.AccessKey.AccessKeyId)
-	fmt.Println("Secret access key:", *resp.AccessKey.SecretAccessKey)
 }
--- s3/s3.go
+++ s3/s3.go
@@ -1,11 +1,10 @@
 package s3
 
 import (
-	"bytes"
 	"context"
-	"errors"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -15,66 +14,114 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
-type S3_info struct {
-	S_AwsS3Region    string
-	S_AwsAccessKey   string
-	S_AwsSecretkey   string
-	S_AwsProfileName string
-	S_BucketName     string
-	S3Client         *s3.Client
+type Info struct {
+	S_Region      string
+	S_Access_Key  string
+	S_Secret_key  string
+	S_Bucket_name string
+	S3_clint      *s3.Client
 }
 
-func (s *S3_info) Get_AccessKey(acccess_key string, secret_key string) {
-	s.S_AwsAccessKey = acccess_key
-	s.S_AwsSecretkey = secret_key
+func (i *Info) Set_session(access_key string, secret_key string, region string) {
+	i.S_Access_Key = access_key
+	i.S_Secret_key = secret_key
+	i.S_Region = region
 }
 
-func (s *S3_info) Set_S3ConfigDefault() error {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return errors.New(err.Error())
-	}
-	s.S3Client = s3.NewFromConfig(cfg)
-
-	return nil
-}
-
-func (s *S3_info) Set_S3ConfigByKey() error {
-	creds := credentials.NewStaticCredentialsProvider(s.S_AwsAccessKey, s.S_AwsSecretkey, "")
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(s.S_AwsS3Region), config.WithCredentialsProvider(creds))
+func (i *Info) Set_s3_config() error {
+	cred := credentials.NewStaticCredentialsProvider(i.S_Access_Key, i.S_Secret_key, "")
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithCredentialsProvider(cred),
+		config.WithRegion(i.S_Region))
 	if err != nil {
 		return err
 	}
-
-	s.S3Client = s3.NewFromConfig(cfg)
+	i.S3_clint = s3.NewFromConfig(cfg)
 
 	return nil
 }
 
-func (s *S3_info) Upload_S3_file(filename, preFix string) *manager.UploadOutput {
-	file, err := ioutil.ReadFile(filename)
-	uploader := manager.NewUploader(s.S3Client)
-	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(s.S_BucketName),
-		Key:    aws.String(filename),
-		Body:   bytes.NewReader(file),
-	})
-	if err != nil {
-		panic(err)
-	}
-	return result
-}
-
-func (s *S3_info) Insert_bucket(bucket_name string, region types.BucketLocationConstraint) {
-	output, err := s.S3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
-		Bucket: &bucket_name,
+func (i *Info) Insert_s3_bucket(new_bucket_name string, region types.BucketLocationConstraint) error {
+	_, err := i.S3_clint.CreateBucket(context.Background(), &s3.CreateBucketInput{
+		Bucket: aws.String(new_bucket_name),
 		CreateBucketConfiguration: &types.CreateBucketConfiguration{
 			LocationConstraint: region,
 		},
 	})
 	if err != nil {
-		panic(err)
+		return err
 	}
-	fmt.Println(output.Location)
+
+	fmt.Printf("Insert bucket: %s\n", new_bucket_name)
+
+	return nil
+}
+
+func (i *Info) Delete_s3_bucket(bucket_name string) error {
+	_, err := i.S3_clint.DeleteBucket(context.Background(), &s3.DeleteBucketInput{
+		Bucket: aws.String(bucket_name),
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Delete bucket: %s\n", bucket_name)
+	return nil
+}
+
+func (i *Info) Get_s3_bucket_list() error {
+	output, err := i.S3_clint.ListBuckets(context.Background(), &s3.ListBucketsInput{})
+	if err != nil {
+		return err
+	}
+
+	for i, bucket := range output.Buckets {
+		fmt.Printf("%d: %s\n", i, *bucket.Name)
+	}
+
+	return nil
+}
+
+func (i *Info) Upload_file(file_name string, folder string, bucket_name string) (*manager.UploadOutput, error) {
+	file, err := os.Open(file_name)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	uploader := manager.NewUploader(i.S3_clint)
+	path_key := filepath.Join(folder, file_name)
+
+	result, err := uploader.Upload(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket_name),
+		Key:    aws.String(path_key),
+		Body:   file,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (i *Info) Download_file(directory string, key string) error {
+	file := filepath.Join(directory, key)
+	if err := os.MkdirAll(filepath.Dir(file), 7750); err != nil {
+		return err
+	}
+
+	fd, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+
+	defer fd.Close()
+
+	downloader := manager.NewDownloader(i.S3_clint)
+	_, err = downloader.Download(context.TODO(), fd, &s3.GetObjectInput{
+		Bucket: &i.S_Bucket_name,
+		Key:    &key,
+	})
+
+	return err
 }
