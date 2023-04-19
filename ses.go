6a3c27523d8a356d5673f76dc7c7a189c9753acf
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-04-19 13:05:49 +0900

    commit

--- ses.go
+++ ses.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-)
+// 	"github.com/aws/aws-sdk-go-v2/config"
+// )
 
-type SES struct {
-	s_region     string
-	s_access_Key string
-	s_secret_key string
-	ses_cfg
-}
+// type SES struct {
+// 	s_region     string
+// 	s_access_Key string
+// 	s_secret_key string
+// 	ses_cfg
+// }
 
-func (c *SES) set_cfg() error {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return err
-	}
+// func (c *SES) set_cfg() error {
+// 	cfg, err := exter
+// 	if err != nil {
+// 		return err
+// 	}
 
-}
+// }
